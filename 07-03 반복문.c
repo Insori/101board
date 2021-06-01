083edf5f1e231cd8555d957465af6c3624360ead
Insori <82927307+Insori@users.noreply.github.com>
2021-06-01 10:09:31 +0900

    [문법공부] while문2

--- 07-03 반복문.c
+++ 07-03 반복문.c
@@ -3,11 +3,11 @@
 void main(void)
 {
 	int num = 0;
-	//무한반복
-	while (1)
+	
+	while (num!=5)
 	{
-		if (num == 5)
-			break;
+		//if (num == 5)
+			//break;
 
 		printf("우리귀살대는언제나너희에게유리한밤의어둠속에서싸우고있어\n");
 		num++;
