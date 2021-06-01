fe8a25d39252bbb12820f67f66e27bb92e1dfee9
Insori <82927307+Insori@users.noreply.github.com>
2021-06-01 10:22:14 +0900

    [문법공부]while문 4

--- 07-03 반복문.c
+++ 07-03 반복문.c
@@ -2,15 +2,27 @@
 
 void main(void)
 {
-	int num = 0;
 	
-	while (num<5)
+	
+	for (int num = 0; num < 5; num++)
+	{
+		//if (num == 5)
+			//break;
+
+		printf("우리귀살대는언제나너희에게유리한밤의어둠속에서싸우고있어\n");
+		
+		
+	}
+	/*
+	int num = 0;
+	while (num < 5)
 	{
 		//if (num == 5)
 			//break;
 
 		printf("우리귀살대는언제나너희에게유리한밤의어둠속에서싸우고있어\n");
 		num++;
-		
+
 	}
+	*/
 }
