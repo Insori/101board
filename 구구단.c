21de00de2f216c9d697362ea2186090476091d6c
Insori <82927307+Insori@users.noreply.github.com>
2021-06-14 14:54:56 +0900

    [문법 공부] 구구단 2단 간단하게 2

--- 구구단.c
+++ 구구단.c
@@ -2,13 +2,14 @@
 
 void main(void)
 {
-	printf("2*1=2\n");
-	printf("2*2=4\n");
-	printf("2*3=6\n");
-	printf("2*4=8\n");
-	printf("2*5=10\n");
-	printf("2*6=12\n");
-	printf("2*8=14\n");
-	printf("2*9=16\n");
-	printf("2*10=18\n");
+	printf("2*1=%d\n",2*1);
+	printf("2*2=%d\n",2*2);
+	printf("2*3=%d\n",2*3);
+	printf("2*4=%d\n",2*4);
+	printf("2*5=%d\n",2*5);
+	printf("2*6=%d\n",2*6);
+	printf("2*7=%d\n", 2 * 7);
+	printf("2*8=%d\n",2*8);
+	printf("2*9=%d\n",2*9);
+	printf("2*10=%d\n",2*10);
 }
