e44b3c4a7d0bb9a0b0b737c8c8df358f87f507fc
Insori <82927307+Insori@users.noreply.github.com>
2021-06-14 15:07:10 +0900

    [문법연습] 구구단 2단 간단하게 4(반복문 사용)

--- 구구단.c
+++ 구구단.c
@@ -3,14 +3,7 @@
 void main(void)
 {
 	int dan = 2;
-	printf("%d*1=%d\n", dan, dan * 1);
-	printf("%d*2=%d\n", dan, dan * 2);
-	printf("%d*3=%d\n", dan, dan * 3);
-	printf("%d*4=%d\n", dan, dan * 4);
-	printf("%d*5=%d\n", dan, dan * 5);
-	printf("%d*6=%d\n", dan, dan * 6);
-	printf("%d*7=%d\n", dan, dan * 7);
-	printf("%d*8=%d\n", dan, dan * 8);
-	printf("%d*9=%d\n", dan, dan * 9);
+	for (int i = 1; i < 10; i++)
+		printf("%d*%d=%d\n", dan, i, dan * i);
 	
 }
