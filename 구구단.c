12a1cdfcfed28574800cfacee28992ccdff762c8
Insori <82927307+Insori@users.noreply.github.com>
2021-06-15 09:50:52 +0900

    [문법 연습] 구구단에 break 추가

--- 구구단.c
+++ 구구단.c
@@ -4,8 +4,16 @@ void main(void)
 {
 	for (int dan = 2; dan <= 9; dan++)
 	{
+		/*if (dan == 4)
+		{
+			break;	//바깥쪽 for문에 대한 break
+		}*/
 		for (int i = 1; i < 10; i++)
 		{
+			if (i == 4)
+			{
+				break;	//안쪽 for문에 대한 break;
+			}
 			printf("%d X %d = %d\n", dan, i, dan * i);
 		}
 		printf("\n");
