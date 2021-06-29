a40b3fe48efad91f8edf670a974fa5816553319a
Insori <82927307+Insori@users.noreply.github.com>
2021-06-29 10:14:46 +0900

    재귀함수 완성

--- 문법 연습/08-04 재귀함수 (recursive, recursion).c
+++ 문법 연습/08-04 재귀함수 (recursive, recursion).c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-int factorial(int num)
+int factorial(int num)	//패러민터
 {
 	if (num == 1)
 		return 1;
+	return num * factorial(num - 1);
 		
 }
 
 void main(void)
 {
-	int val = 1;
+	int val = 2;
 	int a;
 	a = factorial(val);	//아규먼트
 	printf("%d 팩토리얼은 %d\n", val, a);
