d9187985b9c586b6830ec14dd402b95f2097a534
Insori <82927307+Insori@users.noreply.github.com>
2021-08-30 14:11:27 +0900

    한글깨짐제거

---
+++ 문법 연습/성적처리.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int score;
+	printf("점수를 입력하시오 : ");
+	scanf("%d", &score);
+	
+	
+	if (90 <= score && score <= 100)
+		printf("A");
+	else if (80 <= score && score < 90)
+		printf("B");
+	else if (70 <= score && score < 80)
+		printf("C");
+	else if (score < 70)
+		printf("D");
+}
