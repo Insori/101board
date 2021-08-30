d80653bcbac20d98a20e9c020bdf03675a44b662
Insori <82927307+Insori@users.noreply.github.com>
2021-08-31 08:51:07 +0900

    여러 과목 추가

--- 문법 연습/성적처리.c
+++ 문법 연습/성적처리.c
@@ -2,17 +2,52 @@
 
 void main(void)
 {
-	int score;
-	printf("점수를 입력하시오 : ");
-	scanf("%d", &score);
+	int C_score;
+	printf("C언어 점수를 입력하시오 : ");
+	scanf("%d", &C_score);
 	
 	
-	if (90 <= score && score <= 100)
+	if (90 <= C_score && C_score <= 100)
 		printf("A");
-	else if (80 <= score && score < 90)
+	else if (80 <= C_score && C_score < 90)
 		printf("B");
-	else if (70 <= score && score < 80)
+	else if (70 <= C_score && C_score < 80)
 		printf("C");
-	else if (score < 70)
+	else if (C_score < 70)
 		printf("D");
+	
+	printf("\n");
+
+	
+	int Java_score;
+	printf("자바 점수를 입력하시오 : ");
+	scanf("%d", &Java_score);
+
+	if (90 <= Java_score && Java_score <= 100)
+		printf("A");
+	else if (80 <= Java_score && Java_score < 90)
+		printf("B");
+	else if (70 <= Java_score && Java_score < 80)
+		printf("C");
+	else if (Java_score < 70)
+		printf("D");
+	
+	printf("\n");
+
+
+	int Com_score;
+	printf("컴퓨터구조 점수를 입력하시오 : ");
+	scanf("%d", &Com_score);
+
+	if (90 <= Com_score && Com_score <= 100)
+		printf("A");
+	else if (80 <= Com_score && Com_score < 90)
+		printf("B");
+	else if (70 <= Com_score && Com_score < 80)
+		printf("C");
+	else if (Com_score < 70)
+		printf("D");
+
+	printf("\n");
+	
 }
