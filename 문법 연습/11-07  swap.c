d3d564c2af69874aacf913fbf71e1a21cb5439f2
Insori <82927307+Insori@users.noreply.github.com>
2021-10-12 10:10:40 +0900

    주소값을 argumant로 넘겨줘서 swap을 함

---
+++ 문법 연습/11-07  swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void Swap(int* num1, int num2);
+
+void main(void)
+{
+	int a = 10, b = 20;
+	printf("바꾸지 전 a:%d b:%d\n", a,b);
+	Swap(&a,&b);
+	printf("바꾸기 후 a:%d b:%d\n", a, b);
+}
+
+//두 변수의 값을 바꿈
+void Swap(int* num1,int* num2)
+{
+	int temp;	//임자와
+	temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
