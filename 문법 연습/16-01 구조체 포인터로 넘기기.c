4116039894249315f9e5b1c5b337b49ea88ba58e
Insori <82927307+Insori@users.noreply.github.com>
2021-12-21 09:16:05 +0900

    정적할당된 구조체를 포인터로 구현

---
+++ 문법 연습/16-01 구조체 포인터로 넘기기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct {
+	int x;
+	int y;
+}Point;
+
+void main(void)
+{
+	Point a = { 3,4 };
+	Point* ptr = &a;
+
+	//구조체에서 직접접근
+	printf("%d %d\n", a.x,a.y);
+
+	//포인터를 통한 간접접근
+	printf("%d %d\n", (*ptr).x, (*ptr).y);	//연산자 우선순위 때문에 괄호 해줘야 됨
+	
+	//위에 방식이 번거로우므로 (*ptr).을 ptr->로 바꿔줌
+	printf("%d %d\n", ptr->x, ptr->y);
+}
