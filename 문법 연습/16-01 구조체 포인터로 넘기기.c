2e7680901a32753b5a8261c9169fd9082ac5a726
Insori <82927307+Insori@users.noreply.github.com>
2021-12-21 09:22:57 +0900

    동적할당된 구조체를 포인터로 표현

--- 문법 연습/16-01 구조체 포인터로 넘기기.c
+++ 문법 연습/16-01 구조체 포인터로 넘기기.c
@@ -7,15 +7,15 @@ typedef struct {
 
 void main(void)
 {
-	Point a = { 3,4 };
-	Point* ptr = &a;
+	Point* ptr = (Point*)malloc(sizeof(Point));
+	ptr->x = 3;
+	ptr->y = 4;
 
-	//구조체에서 직접접근
-	printf("%d %d\n", a.x,a.y);
+	////구조체에서 직접접근
+//	printf("%d %d\n", a.x,a.y);
 
 	//포인터를 통한 간접접근
 	printf("%d %d\n", (*ptr).x, (*ptr).y);	//연산자 우선순위 때문에 괄호 해줘야 됨
-	
 	//위에 방식이 번거로우므로 (*ptr).을 ptr->로 바꿔줌
 	printf("%d %d\n", ptr->x, ptr->y);
 }
